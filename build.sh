5ad0c15714cbee2277ed23c253e6bf0540e5821f
TLeugazy Yerassyl <tleugazy98@gmail.com>
2022-02-23 23:08:32 +0600

    sdsddsd

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ touch /var/app/current/go.bak
 sudo rm /var/app/current/go.*
 
 # create the application binary that eb uses
-GOOS=linux GOARCH=amd64 go build -o bin/application -ldflags="-s -w"
+GOOS=linux GOARCH=amd64 go build -o bin/main -ldflags="-s -w"
